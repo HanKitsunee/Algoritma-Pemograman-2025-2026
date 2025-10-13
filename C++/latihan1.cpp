5727f82e945b4e4bd82c10ba366f7b80c55a3704
HanKitsunee <han.kitsu.0517@gmail.com>
2025-10-13 10:44:30 +0700

    Latihan 1 C++

---
+++ C++/latihan1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Farhan";
+    int umur = 19;
+    float tinggi = 162;
+    bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Status aktif: " << aktif << endl;
+
+    return 0;
+}
